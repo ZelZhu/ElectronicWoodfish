61fa92918a10233d221a545441524d2d11204c17
Zel <zel9696@outlook.com>
2022-11-18 13:49:43 +0800

    暂时完工

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,6 +22,7 @@ namespace ElectronicWoodfish
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,19 +31,95 @@ namespace ElectronicWoodfish
         public int NowGd = 0;
         private void InitWindows()
         {
-            this.ResizeMode = ResizeMode.CanMinimize;       
+            ResizeMode = ResizeMode.CanMinimize;
         }
 
-        private void MuYu_MouseDown(object sender, MouseButtonEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            NowGd++;
-            ShowGD();
+
+            ShowAnimation();
+            ShowGDJY();
         }
 
+        /// <summary>
+        /// 展示功德
+        /// </summary>
         private void ShowGD()
         {
             Gd_Number.Content = NowGd;
         }
+
+        /// <summary>
+        /// 播放敲击声 dududu
+        /// </summary>
+        private static void PlayDuDuDU()
+        {
+            var soundPlayer = new SoundPlayer($"{Environment.CurrentDirectory}/mp3/du.mp3");
+            soundPlayer.Play();
+        }
+
+        /// <summary>
+        /// 缩放恢复
+        /// </summary>
+        private void ShowAnimation()
+        {
+            var isShow = false;
+
+            if (!isShow)
+            {
+                isShow = true;
+                var sb = new Storyboard();
+                var anima = new DoubleAnimation()
+                {
+                    By = -30,
+                    Duration = TimeSpan.FromMilliseconds(70),
+                    AutoReverse = true,
+                };
+
+                Storyboard.SetTarget(anima, MuYu);
+                Storyboard.SetTargetProperty(anima, new PropertyPath("Width"));
+                Storyboard.SetTargetProperty(anima, new PropertyPath("Height"));
+                sb.Children.Add(anima);
+                sb.Begin();
+
+                NowGd++;
+                PlayDuDuDU();
+                ShowGD();
+                isShow = false;
+            }
+
+
+        }
+        private void ShowGDJY()
+        {
+
+            var sb = new Storyboard();
+            var marginAnim = new ThicknessAnimation()
+            {
+                From = new Thickness(690, 142, 0, 0),
+                To = new Thickness(690, 82, 0, 0),
+                Duration = TimeSpan.FromMilliseconds(300),
+            };
+
+            var opaAnima = new DoubleAnimation()
+            {
+                From = 1,
+                To = 0,
+                Duration = TimeSpan.FromMilliseconds(300),
+            };
+            Storyboard.SetTarget(marginAnim, LBL_GDJY);
+            Storyboard.SetTarget(opaAnima, LBL_GDJY);
+
+            Storyboard.SetTargetProperty(marginAnim, new PropertyPath("Margin"));
+            Storyboard.SetTargetProperty(opaAnima, new PropertyPath("Opacity"));
+
+            sb.Children.Add(marginAnim);
+            sb.Children.Add(opaAnima);
+
+            sb.Begin();
+
+        }
+
     }
-    
+
 }
